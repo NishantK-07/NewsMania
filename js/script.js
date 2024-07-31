145861726e56d642bc1e4203f9085f4f9dbe3dea
NISHANT KUMAR <nishantkumar19404@gmail.com>
2024-07-31 15:10:25 +0530

    changed script

--- js/script.js
+++ js/script.js
@@ -29,8 +29,7 @@ toggleBtn.addEventListener('click', () => {
 //fetching news
 fetchnews()
 async function fetchnews(){
-    const url=`
-https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c9fac730ef284886a1be6dc1858033eb`;
+    const url=`https://newsapi.org/v2/everything?q=tesla&from=2024-06-30&sortBy=publishedAt&apiKey=c9fac730ef284886a1be6dc1858033eb`;
     const response= await fetch(url);
             if(response.status==400){
                 alert("location is invalid");
